4fd9592f1126057cad7a9f92fc542e4bbd07513f
comtel <comtel2000@googlemail.com>
2012-09-02 19:50:31 +0200

    0.0.9 rc1 binary added

--- src/purejavacomm/PureJavaComm.java
+++ src/purejavacomm/PureJavaComm.java
@@ -3,7 +3,7 @@ package purejavacomm;
 public class PureJavaComm {
 	// change this if you modify the code and re-distribute
 	public static String getVersion() {
-		return "0.0.8_rc1";
+		return "0.0.9_rc1";
 	}
 
 	// change this is if you fork the project
